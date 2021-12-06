bed90f605b38bc6d0f54749288cabed448442c28
Pete Platt <pete_platt@hotmail.com>
2021-12-06 22:05:36 +0000

    Add Python solution for day 02

---
+++ 02/python/magnitude.py
@@ -0,0 +1,36 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+
+horiz = 0
+depth = 0
+for command in lines:
+    parts = command.split(' ')
+    magnitude = int(parts[1])
+    if parts[0] == "forward":
+        horiz += magnitude
+    if parts[0] == "backward":
+        horiz -= magnitude
+    if parts[0] == "down":
+        depth += magnitude
+    if parts[0] == "up":
+        depth -= magnitude
+print("Depth %d pos %d - %d" % (depth, horiz, depth * horiz))
+
+# Part 2
+horiz = 0
+depth = 0
+aim = 0
+for command in lines:
+    parts = command.split(' ')
+    magnitude = int(parts[1])
+    if parts[0] == "forward":
+        horiz += magnitude
+        depth += (aim * magnitude)
+    if parts[0] == "backward":
+        horiz -= magnitude
+        depth -= (aim * magnitude)
+    if parts[0] == "down":
+        aim += magnitude
+    if parts[0] == "up":
+        aim -= magnitude
+print("Depth %d pos %d - %d" % (depth, horiz, depth * horiz))
