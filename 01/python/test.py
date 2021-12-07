442d994ecbcb78d0835442e23d99a990ac3c0927
Pete Platt <flyofkodos@aol.com>
2021-12-07 16:36:36 +0000

    Add unit test for day 01 python solution

---
+++ 01/python/test.py
@@ -0,0 +1,17 @@
+import unittest
+from sonar import part1, part2
+
+
+class Test_day02_test(unittest.TestCase):
+
+    def test_part1(self):
+        test_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        self.assertEqual(7, part1(test_data))
+
+    def test_part2(self):
+        test_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        self.assertEqual(5, part2(test_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
