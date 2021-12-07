b43e99741b0dec7eb17e98c876b4f12ac1e4d195
Pete Platt <flyofkodos@aol.com>
2021-12-07 16:05:29 +0000

    Add python unit test

---
+++ 02/python/test.py
@@ -0,0 +1,19 @@
+import unittest
+from magnitude import part1, part1
+
+
+class Test_day02_test(unittest.TestCase):
+
+    def test_part1(self):
+        test_data = ["forward 5", "down 5",
+                     "forward 8", "up 3", "down 8", "forward 2"]
+        self.assertEqual(150, part1(test_data))
+
+    def test_part2(self):
+        test_data = ["forward 5", "down 5",
+                     "forward 8", "up 3", "down 8", "forward 2"]
+        self.assertEqual(900, part2(test_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
