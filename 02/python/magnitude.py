69f6d56e8d9b5308126b3757e28647b6b4a0d472
flyofkodos <41906010+flyofkodos@users.noreply.github.com>
2021-12-07 16:13:03 +0000

    Python tests (#5)
    * Add python unit test
    
    * add python unit tests
    
    * Split python actions to tests and run

--- 02/python/magnitude.py
+++ 02/python/magnitude.py
@@ -1,38 +1,55 @@
 import os
-file_path = os.path.join(os.path.dirname(__file__), "input.txt")
-with open(file_path) as f:
-    lines = f.readlines()
+import sys
 
-horiz = 0
-depth = 0
-for command in lines:
-    parts = command.split(' ')
-    magnitude = int(parts[1])
-    if parts[0] == "forward":
-        horiz += magnitude
-    if parts[0] == "backward":
-        horiz -= magnitude
-    if parts[0] == "down":
-        depth += magnitude
-    if parts[0] == "up":
-        depth -= magnitude
-print("Depth %d pos %d - %d" % (depth, horiz, depth * horiz))
 
-# Part 2
-horiz = 0
-depth = 0
-aim = 0
-for command in lines:
-    parts = command.split(' ')
-    magnitude = int(parts[1])
-    if parts[0] == "forward":
-        horiz += magnitude
-        depth += (aim * magnitude)
-    if parts[0] == "backward":
-        horiz -= magnitude
-        depth -= (aim * magnitude)
-    if parts[0] == "down":
-        aim += magnitude
-    if parts[0] == "up":
-        aim -= magnitude
-print("Depth %d pos %d - %d" % (depth, horiz, depth * horiz))
+def part1(commands):
+    horiz = 0
+    depth = 0
+    for command in commands:
+        parts = command.split(' ')
+        magnitude = int(parts[1])
+        if parts[0] == "forward":
+            horiz += magnitude
+        if parts[0] == "backward":
+            horiz -= magnitude
+        if parts[0] == "down":
+            depth += magnitude
+        if parts[0] == "up":
+            depth -= magnitude
+    print("Depth %d pos %d - %d" % (depth, horiz, depth * horiz))
+    return depth * horiz
+
+
+def part2(commands):
+
+    # Part 2
+    horiz = 0
+    depth = 0
+    aim = 0
+    for command in commands:
+        parts = command.split(' ')
+        magnitude = int(parts[1])
+        if parts[0] == "forward":
+            horiz += magnitude
+            depth += (aim * magnitude)
+        if parts[0] == "backward":
+            horiz -= magnitude
+            depth -= (aim * magnitude)
+        if parts[0] == "down":
+            aim += magnitude
+        if parts[0] == "up":
+            aim -= magnitude
+    print("Depth %d pos %d - %d" % (depth, horiz, depth * horiz))
+    return depth * horiz
+
+
+def main():
+    file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+    with open(file_path, "+rt") as f:
+        lines = f.readlines()
+    part1(lines)
+    part2(lines)
+
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
