1b0422c9fa8e0e8ed6334cfda8724f3f468510b6
flyofkodos <41906010+flyofkodos@users.noreply.github.com>
2021-12-21 16:17:43 +0000

    Develop day17 (#22)
    * Day 17 Part 1 Solution
    
    Add the solution code for part 1 of the puzzle.
    
    * Fix workflow path for day 17

---
+++ 17/dotnet6/probe.cs
@@ -0,0 +1,20 @@
+var path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+path = Path.GetDirectoryName(path);
+var input = File.ReadAllText($"{path}\\Day17Input.txt");
+Console.WriteLine($"Part 1 is {Part1(input)}");
+Console.WriteLine($"Part 2 is {Part2(input)}");
+
+int Part1(string lines)
+{
+    var temp = lines.Split(':')[1].Split(',');
+    var ystuff = temp[1].Split('=')[1].Split('.', StringSplitOptions.RemoveEmptyEntries);
+    var top = int.Parse(ystuff[0]);
+    var dy = (-top) - 1; // Highest value to end at the top line of the target area
+    return (dy * (dy + 1) / 2); // Return the sum of the positive vertical velocities
+}
+
+int Part2(string input)
+{
+    // TODO
+    return -1;
+}
