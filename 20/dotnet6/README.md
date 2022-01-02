74b56a0633a78a666f2cb7114d0a3ebe2e0aaa38
Pete Platt <flyofkodos@aol.com>
2022-01-02 20:04:56 +0000

    Add Day 20 Solution.
    Add the code for Day 20.
    Add project to base sln file.

---
+++ 20/dotnet6/README.md
@@ -0,0 +1,3 @@
+# Advent of Code Day 21  
+
+The C# version of the solution for the [Advent of Code 2021 day 20 puzzle](https://adventofcode.com/2021/day/20)  
---
+++ 20/dotnet6/enhance.cs
@@ -0,0 +1,54 @@
+var path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+path = Path.GetDirectoryName(path);
+var input = File.ReadAllLines($"{path}\\Day20Input.txt");
+Console.WriteLine($"Part 1 is {Part1(input, 2)}");
+Console.WriteLine($"Part 2 is {Part1(input, 50)}");
+
+int Part1(string[] lines, int Passes)
+{
+    var algorithm = lines[0];
+    var pixels = new List<(int x, int y)>();
+    for (var y = 2; y < lines.Length; y++)
+    {
+        var tempLine = lines[y].ToCharArray();
+        for (var x = 0; x < tempLine.Length; x++)
+        {
+            if (tempLine[x] == '#')
+            {
+                pixels.Add((x, y - 2));
+            }
+        }
+    }
+
+    var newPixels = new List<(int x, int y)>();
+    for (var loop = 0; loop < Passes; loop++)
+    {
+        var startx = -(Passes * 2) + loop;
+        var endx = lines[^1].Length + (Passes * 2) - loop;
+        var starty = -(Passes * 2) + loop;
+        var endy = lines.Length + (Passes * 2) - loop;
+        for (var px = startx; px <= endx; px++)
+        {
+            for (var py = starty; py <= endy; py++)
+            {
+                var square = pixels.Where(t =>
+                    t.x >= px - 1 && t.x <= px + 1 && t.y >= py - 1 && t.y <= py + 1).ToList();
+                var bin = 0;
+                foreach (var (x, y) in square)
+                {
+                    bin += 1 << ((py - y + 1) * 3 + px - x + 1);
+                }
+
+                if (algorithm[bin] == '#')
+                {
+                    newPixels.Add((px, py));
+                }
+            }
+        }
+
+        pixels.Clear();
+        pixels.AddRange(newPixels);
+        newPixels.Clear();
+    }
+    return pixels.Count;
+}
